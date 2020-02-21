ac0e812441b0a907f109d202aad0ee73d3fcc82e
ssloveca <ss.loveca18@gmail.com>
2020-02-21 15:30:05 +0800

    First Commit

--- Blog-Project-MVC/App_Start/RouteConfig.cs
+++ Blog-Project-MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Blog_Project_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Blog", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Blog-Project-MVC/Controllers/BlogController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog_Project_MVC.Controllers
+{
+   
+
+    public class BlogController : Controller
+    {
+        BlogEntities nd = new BlogEntities();
+
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var blogdetails = nd.Blogs.ToList().OrderByDescending(x=>x.Bid);
+            return View(blogdetails);
+        }
+
+        public ActionResult UploadBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadBlog(Blog bg)
+        {
+           
+            nd.Blogs.Add(bg);
+            nd.SaveChanges();
+
+            ViewBag.message = "Blog Details are saved successfully!";
+            return View();
+        }
+
+        public ActionResult Food()
+        {
+            var foodarticle = nd.Blogs.Where(x => x.BCategory == "Food");
+            return View(foodarticle);
+        }
+
+        public ActionResult Sports()
+        {
+            var sportsarticle = nd.Blogs.Where(x => x.BCategory == "Sports");
+            return View(sportsarticle);
+        }
+
+        public ActionResult Movies()
+        {
+            var moviesarticle = nd.Blogs.Where(x => x.BCategory == "Movies");
+            return View(moviesarticle);
+        }
+
+        public ActionResult Recipesworld()
+        {
+            return View();
+        }
+    }
+}
